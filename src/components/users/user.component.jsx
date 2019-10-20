da0448754da7012f11ff007b7a1cc3291e2d035a
findforhad <forhadhossenbabu@gmail.com>
2019-10-20 12:04:52 +0600

    User Class to Function With useEffect Hook

--- src/components/users/user.component.jsx
+++ src/components/users/user.component.jsx
@@ -1,103 +1,102 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import Repos from "../repos/repos.component";
 import Spinner from "../layout/spinner.component";
 
-class User extends React.Component {
-  async componentDidMount() {
-    await this.props.getUser(this.props.match.params.login);
-    await this.props.getUserRepos(this.props.match.params.login);
+const User = ({ loading, user, repos, match, getUser, getUserRepos }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
+
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+    company
+  } = user;
+
+  if (loading) {
+    return <Spinner />;
   }
 
-  render() {
-    const { loading, user, repos } = this.props;
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-      company
-    } = user;
-    if (loading) {
-      return <Spinner />;
-    }
-    return (
-      <React.Fragment>
-        <Link to="/" className="btn btn-light">
-          Back to search
-        </Link>
-        Hireable :{" "}
-        {hireable ? (
-          <label style={{ color: "green" }}>&#10003;</label>
-        ) : (
-          <label style={{ color: "red" }}>&#10005;</label>
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              alt={name}
-              className="round-img"
-              style={{ width: "150px" }}
-            />
-            <h1>{name}</h1>
-            <p>Location : {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <React.Fragment>
-                <h3>Profile Bio</h3>
-                <p>{bio}</p>
-              </React.Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit GitHub Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <React.Fragment>
-                    <strong>Username: </strong> {login}
-                  </React.Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <React.Fragment>
-                    <strong>Companey: </strong> {company}
-                  </React.Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <React.Fragment>
-                    <strong>Website: </strong> <a href={blog}>{blog}</a>
-                  </React.Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+  return (
+    <React.Fragment>
+      <Link to="/" className="btn btn-light">
+        Back to search
+      </Link>
+      Hireable :{" "}
+      {hireable ? (
+        <label style={{ color: "green" }}>&#10003;</label>
+      ) : (
+        <label style={{ color: "red" }}>&#10005;</label>
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt={name}
+            className="round-img"
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location : {location}</p>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers : {followers}</div>
-          <div className="badge badge-success">Following : {following}</div>
-          <div className="badge badge-light">Public Repos : {public_repos}</div>
-          <div className="badge badge-success">
-            Public Gists : {public_gists}
-          </div>
+        <div>
+          {bio && (
+            <React.Fragment>
+              <h3>Profile Bio</h3>
+              <p>{bio}</p>
+            </React.Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit GitHub Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <React.Fragment>
+                  <strong>Username: </strong> {login}
+                </React.Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <React.Fragment>
+                  <strong>Companey: </strong> {company}
+                </React.Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <React.Fragment>
+                  <strong>Website: </strong> <a href={blog}>{blog}</a>
+                </React.Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers : {followers}</div>
+        <div className="badge badge-success">Following : {following}</div>
+        <div className="badge badge-light">Public Repos : {public_repos}</div>
+        <div className="badge badge-success">Public Gists : {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </React.Fragment>
+  );
+};
+
 export default User;
